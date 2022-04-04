e4645b815f40b4ac8c594e3e574679511173cdb5
fanshiliutiandian <2891880075@qq.com>
2022-04-04 23:24:32 +0800

    第一次提交

---
+++ src/com/atguigu/com/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.atguigu.com;
+
+class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("hello java_world!");
+
+
+
+        char a='a';
+        char b=97;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        int i1=(int)a;
+        System.out.println("i1="+i1);
+        System.out.print("i1="+i1);
+        System.out.print("i1="+i1);
+    }
+}
---
+++ src/com/atguigu/com/NumberTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.com;
+
+public class NumberTest {
+    public static void main(String args[]){
+        int i=20;
+        int j=30;
+        System.out.println(i=j);
+        System.out.println(i==j);
+        System.out.println(2<<4);
+
+        System.out.println(89>>9);
+        
+
+    }
+
+}
